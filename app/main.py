c763ffb4f78e625a1f92828682f50148b96c4e88
Mike Fotinakis <mike@fotinakis.com>
2015-08-24 12:56:33 -0700

    PEP8 formatting with custom 2-space indent.

--- app/main.py
+++ app/main.py
@@ -9,55 +9,55 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    jobs = jobs_service.list_jobs()
-    builds = jobs_service.list_builds(limit=20)
-    return flask.render_template('index.html', builds=builds, jobs=jobs)
+  jobs = jobs_service.list_jobs()
+  builds = jobs_service.list_builds(limit=20)
+  return flask.render_template('index.html', builds=builds, jobs=jobs)
 
 
 @app.route('/builds')
 def builds():
-    builds = jobs_service.list_builds()
-    return flask.render_template('builds.html', builds=builds)
+  builds = jobs_service.list_builds()
+  return flask.render_template('builds.html', builds=builds)
 
 
 @app.route('/jobs')
 def jobs():
-    jobs = jobs_service.list_jobs()
-    return flask.render_template('jobs.html', jobs=jobs)
+  jobs = jobs_service.list_jobs()
+  return flask.render_template('jobs.html', jobs=jobs)
 
 
 @app.route('/builds/<int:build_id>')
 def build(build_id):
-    build = jobs_service.get_build(build_id)
-    return flask.render_template('build.html', build=build)
+  build = jobs_service.get_build(build_id)
+  return flask.render_template('build.html', build=build)
 
 # API.
 
 @app.route('/api/jobs', methods=['POST'])
 def create_job():
-    git_url = request.args.get('git_url')
-    assert git_url
-    job_id = jobs_service.create_job(git_url=git_url)
-    return flask.jsonify({'success': True, 'job_id': job_id})
+  git_url = request.args.get('git_url')
+  assert git_url
+  job_id = jobs_service.create_job(git_url=git_url)
+  return flask.jsonify({'success': True, 'job_id': job_id})
 
 
 @app.route('/api/jobs/<int:job_id>/run', methods=['GET', 'POST'])
 def run_job(job_id):
-    ref = request.args.get('ref')
-    commit_sha = request.args.get('commit_sha')
-    if ref and commit_sha:
-        # Trigger build of single ref and commit SHA.
-        build_id = jobs_service.enqueue_build(job_id, ref, commit_sha)
-        return flask.jsonify({'success': True, 'build_id': build_id, 'message': 'Build enqueued.'})
+  ref = request.args.get('ref')
+  commit_sha = request.args.get('commit_sha')
+  if ref and commit_sha:
+    # Trigger build of single ref and commit SHA.
+    build_id = jobs_service.enqueue_build(job_id, ref, commit_sha)
+    return flask.jsonify({'success': True, 'build_id': build_id, 'message': 'Build enqueued.'})
+  else:
+    # Update refs and trigger all builds.
+    build_ids = jobs_service.run_job(job_id)
+    if build_ids:
+      message = 'Refs changed, enqueued %s builds.' % len(build_ids)
     else:
-        # Update refs and trigger all builds.
-        build_ids = jobs_service.run_job(job_id)
-        if build_ids:
-            message = 'Refs changed, enqueued %s builds.' % len(build_ids)
-        else:
-            message = 'No refs changed, nothing to build.'
-        return flask.jsonify({'success': True, 'message': message})
+      message = 'No refs changed, nothing to build.'
+    return flask.jsonify({'success': True, 'message': message})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
--- app/worker.py
+++ app/worker.py
@@ -6,23 +6,24 @@ import jobs_service
 
 heard_interrupt = False
 
+
 def main():
-    print 'Waiting for builds...'
-    while not heard_interrupt:
-        build = jobs_service.pop_next_build()
-        if not build:
-            time.sleep(2)
-            continue
-        print 'Running build: %s %s %s %s' % (build.id, build.git_url, build.ref, build.commit_sha)
-        result = jobs_service.run_build(build.id)
-        print 'Build %s: %s' % (build.id, 'success' if result else 'failed')
+  print 'Waiting for builds...'
+  while not heard_interrupt:
+    build = jobs_service.pop_next_build()
+    if not build:
+      time.sleep(2)
+      continue
+    print 'Running build: %s %s %s %s' % (build.id, build.git_url, build.ref, build.commit_sha)
+    result = jobs_service.run_build(build.id)
+    print 'Build %s: %s' % (build.id, 'success' if result else 'failed')
 
 
 def sigint_handler(signal, frame):
-    print 'Shutting down...'
-    global heard_interrupt
-    heard_interrupt = True
+  print 'Shutting down...'
+  global heard_interrupt
+  heard_interrupt = True
 signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == '__main__':
-    main()
+  main()
