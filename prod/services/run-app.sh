0f7d505a1417fb97187648731ec79906a427a451
Mike Fotinakis <mike@fotinakis.com>
2015-08-24 18:14:44 -0700

    Checkpoint productionization work.

--- prod/services/run-app.sh
+++ prod/services/run-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Run the process as the "app" user and redirect stdout/stderr to syslog.
-/sbin/setuser app twistd -n web --port 8080 --wsgi main.app 2>&1 | logger -p user.error
+cd /home/app
+/sbin/setuser app twistd -n web --port 8080 --wsgi main.app 2>&1 | logger -p user.info
