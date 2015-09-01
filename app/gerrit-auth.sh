7dc6214a0159f4fccbbfc653521aa5f11fa45d4e
Mike Fotinakis <mike@fotinakis.com>
2015-08-31 19:06:08 -0700

    Add gerrit auth on server startup for ls-remote. :|

---
+++ app/gerrit-auth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /tmp
+git clone https://gerrit.googlesource.com/gcompute-tools
+./gcompute-tools/git-cookie-authdaemon
--- prod/run-app.sh
+++ prod/run-app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Init gerrit credentials once on load.
+# TODO(fotinakis): kill this because it's insecure in the long run, do per-job auth credentials.
+/sbin/setuser app bash /home/app/gerrit-auth.sh
+
 # Run the process as the "app" user and redirect stdout/stderr to syslog.
 cd /home/app
 /sbin/setuser app twistd -n web --port 8080 --wsgi main.app 2>&1 | logger -p user.info
