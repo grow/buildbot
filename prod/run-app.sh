17be0c6d25edb8038617548be76e756a684c2040
Mike Fotinakis <mike@fotinakis.com>
2015-11-06 15:16:18 -0800

    Add nginx proxy in front of twistd for client cert verification.

--- prod/run-app.sh
+++ prod/run-app.sh
@@ -6,4 +6,4 @@
 
 # Run the process as the "app" user and redirect stdout/stderr to syslog.
 cd /home/app
-/sbin/setuser app twistd -n web --port 8080 --wsgi main.app 2>&1 | logger -p user.info
+/sbin/setuser app twistd -n web --port 8000 --wsgi main.app 2>&1 | logger -p user.info
